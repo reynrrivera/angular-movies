65bbc7871ce1774ee07b59666d5c6ed919bee082
Reyner Rivera <reyner.garcia.rivera@gmail.com>
2024-01-08 23:06:03 +0800

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  imageUrl = "https://dotnetnow.net/images/movies/movie-1.jpeg";
+  imageUrl = "assets/5lc1pb.jpg";
+
+  widthUrl = 400;
 
   title = 'UMAY LODICAKES';
+
+  attributeValue = "movie-app-test-value";
+
+  colors = 'styleBgColor styleTextColor';
+
+  styles = 'background-color: blue; color: white;';
+
+  backgroundColor = 'blue';
+
+  fullName = "";
+
+  timeNow = new Date();
+
+  setTimeToNow(){
+    this.timeNow = new Date();
+  }
+
+  color = '';
+  setTextColor(_color: string){
+    console.log(_color);
+    this.color = _color;
+  }
 
   date = new Date();
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoaderComponent } from './components/loader/loader.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { profitPipe } from './pipes/profit.pipe';
 import { pipe } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { pipe } from 'rxjs';
     profitPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
